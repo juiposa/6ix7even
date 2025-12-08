6d5be0c69f0c92ba2e706953326699b39e9d58f2
Juiposa <jeffrey.kapp@gmail.com>
2025-12-08 17:29:54 -0500

    Initial commit

---
+++ 6ix7even/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Configuration;
+
+namespace _6ix7even;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 0;
+
+    public bool IsConfigWindowMovable { get; set; } = true;
+    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+
+    // The below exist just to make saving less cumbersome
+    public void Save()
+    {
+        Plugin.PluginInterface.SavePluginConfig(this);
+    }
+}
